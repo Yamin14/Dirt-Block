67b8393a8d7f6f51ffdac615a51a44b7d0d343dc
Yamin Kazmi <77500567+Yamin14@users.noreply.github.com>
2021-02-04 10:25:07 +0500

    Create DirtBlock.py

---
+++ DirtBlock.py
@@ -0,0 +1,25 @@
+#3d cube
+
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((700, 1400))
+brown = (101, 67, 33)
+green = (0, 255, 0)
+
+running = True
+while running:
+	screen. fill((0, 0, 0))
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+
+	front = pygame.draw.rect(screen, brown, (200, 500, 300, 300))
+	
+	top = pygame.draw.polygon(screen, green, [(200, 500), (358, 345), (658, 345), (500, 500)])
+	
+	side = pygame.draw.polygon(screen, brown, [(658, 345), (658, 645), (500, 800), (500, 500)])
+	
+	line = pygame.draw.line(screen, (0, 0, 0), (500, 500), (500, 800))
+
+	pygame.display.flip()
+pygame.quit()
